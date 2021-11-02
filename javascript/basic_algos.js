3a2da4710f71671e627978f5954112e60c0a5601
Jeff-Clapper <jwclapper@gmail.com>
2021-11-02 19:22:59 -0400

    ran through basic js string algos

--- javascript/basic_algos.js
+++ javascript/basic_algos.js
@@ -112,4 +112,69 @@ function isCreditCardValid(digitArr){
     }
 }
 
-//next algo
+//Remove Blanks: Create a function that, given a string, returns all of that string’s contents, but without blanks. If given the string " Pl ayTha tF u nkyM usi c ", return "PlayThatFunkyMusic".
+function removeBlanks(phrase){
+    phrase = phrase.split("")
+    for(var index = phrase.length-1; index >= 0; index--){
+        if (phrase[index] == " ") {
+            for(var ind = index; ind < phrase.length; ind++) {
+                phrase[ind] = phrase[ind+1];
+            }
+            phrase.length -= 1 
+        }
+    }
+    phrase = phrase.join("")
+    return phrase
+}
+
+//Get Digits: Create a JavaScript function that given a string, returns the integer made from the string’s digits. Given "0s1a3y5w7h9a2t4?6!8?0", the function should return the number 1357924680.
+function getDigits(phrase){
+    phrase.split("");
+    results = []
+    for(var index = 0; index < phrase.length; index++) {
+        if(parseInt(phrase[index]) % 1==0){
+            results.length += 1
+            results[results.length-1] = phrase[index]
+        }
+    }
+    results = results.join("")
+    results = parseInt(results)
+    return results
+}
+
+//Acronyms: Create a function that, given a string, returns the string’s acronym (first letters only, capitalized). Given " there's no free lunch - gotta pay yer way. ", return "TNFL-GPYW". Given "Live from New York, it's Saturday Night!", return "LFNYISN".
+function acronyms(phrase) {
+    phrase = phrase.split(" ")
+    results = []
+    for(var ind = 0;ind < phrase.length; ind++) {
+        results.push(phrase[ind][0].toUpperCase())
+    }
+    results = results.join("")
+    return results
+}
+
+//Count Non-Spaces: Accept a string and return the number of non-space characters found in the string. For example, given "Honey pie, you are driving me crazy", return 29 (not 35).
+function countNonSpaces(phrase) {
+    var count = 0;
+    for(var index = 0;index < phrase.length; index++) {
+        if (phrase[index] !== " ") {
+            count++
+        }
+    }
+    return count
+}
+
+//Remove Shorter Strings: Given a string array and value (length), remove any strings shorter than the length from the array. 
+function removeShorterStrings(phrase,length) {
+    phrase = phrase.split(" ");
+    console.log(phrase)
+    for(var index = phrase.length-1; index >= 0; index--) {
+        if(phrase[index].length < length){
+            for(var ind = index; ind < phrase.length-1; ind++){
+                phrase[ind] = phrase[ind+1];
+            }
+            phrase.length -=1
+        }
+    }
+    return phrase.join(" ")
+}
